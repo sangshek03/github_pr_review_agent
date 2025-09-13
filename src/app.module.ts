56eb38dc96135ef11e9383d13a76eabcde7ec0cd
sangshek03 <abhishekverma5541@gmail.com>
2025-09-13 17:38:04 +0530

    done: db connection

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppConfig, DatabaseConfig } from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Version1Module } from './version1/version1.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [AppConfig, DatabaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+      inject: [ConfigService],
+    }),
+    Version1Module
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/version1/version1.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
+
+@Module({
+  imports: [
+    RouterModule.register([
+    ]),
+  ],
+  providers: [],
+})
+export class Version1Module {}
